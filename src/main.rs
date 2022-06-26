3a5fb03db9181ac4efc9c7e82cb7281e36ae3e10
Sebastian Sastre <sebastian.sastre@telna.com>
2022-06-26 11:59:17 -0300

    refactored socket handler function

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,19 @@
 // #![feature(path)]
 
 use std::io::Read;
-use std::os::unix::net::{UnixListener, UnixStream};
+// use std::net::SocketAddr;
+use std::os::unix::net::{UnixListener, UnixStream, SocketAddr};
 use std::path::Path;
 use std::{thread, fs};
 
 pub static SOCKET_PATH: &'static str = "/tmp/memoizer-socket";
+
+fn on_socket_accept(mut stream: UnixStream, addr: SocketAddr){
+    println!("Accepting incoming connection: {:?}", addr);
+    let mut message = String::new();
+    stream.read_to_string(&mut message);
+    println!("Client said: {}", message);
+}
 
 fn main() {
     let socket = Path::new(SOCKET_PATH);
@@ -26,10 +34,7 @@ fn main() {
 
     match listener.accept() {
         Ok((mut stream, addr)) => {
-            println!("Accepting incoming connection: {:?}", addr);
-            let mut message = String::new();
-            stream.read_to_string(&mut message);
-            println!("Client said: {}", message);
+            on_socket_accept(stream, addr)
         }
         Err(e) => println!("accept function failed: {e:?}"),
     }
