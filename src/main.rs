6ce72c5c31443a45896b99fed81246d005eefbec
Sebastian Sastre <sebastian.sastre@telna.com>
2022-06-26 12:26:06 -0300

    spawning on accept

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,19 @@
 // #![feature(io)]
 // #![feature(path)]
 
-use std::io::Read;
-// use std::net::SocketAddr;
-use std::os::unix::net::{UnixListener, UnixStream, SocketAddr};
+use std::io::{BufRead, BufReader};
+use std::os::unix::net::{UnixListener, UnixStream};
 use std::path::Path;
-use std::{thread, fs};
+use std::{fs, thread};
 
 pub static SOCKET_PATH: &'static str = "/tmp/memoizer-socket";
 
-fn on_socket_accept(mut stream: UnixStream, addr: SocketAddr){
-    println!("Accepting incoming connection: {:?}", addr);
-    let mut message = String::new();
-    stream.read_to_string(&mut message);
-    println!("Client said: {}", message);
+fn on_socket_accept(mut stream: UnixStream) {
+    println!("Accepting incoming connection: {:?}", stream.local_addr());
+    let receiver = BufReader::new(stream);
+    for line in receiver.lines() {
+        println!("{}", line.unwrap());
+    }
 }
 
 fn main() {
@@ -32,10 +32,15 @@ fn main() {
 
     println!("Server started, waiting for clients");
 
-    match listener.accept() {
-        Ok((mut stream, addr)) => {
-            on_socket_accept(stream, addr)
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                thread::spawn(|| on_socket_accept(stream));
+            }
+            Err(err) => {
+                println!("Error: {}", err);
+                break;
+            }
         }
-        Err(e) => println!("accept function failed: {e:?}"),
     }
 }
