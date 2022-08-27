9084dec44e895ac441c0e7d65096dbd74d7f140c
Sebastian Sastre <sebastian.sastre@telna.com>
2022-08-27 15:24:03 -0300

    processing input and sending response from on_socket_accept

--- src/listener.rs
+++ src/listener.rs
@@ -1,7 +1,9 @@
-use std::cell::{RefCell, RefMut};
+
+
+use std::error::Error;
 
 pub(crate) use serde::{Deserialize, Serialize};
-use serde_json::{self, Result, Value};
+use serde_json::{self, Value};
 use tokio::io::{AsyncBufReadExt, AsyncWriteExt};
 use tokio::{io::BufReader, net::TcpStream};
 
@@ -15,75 +17,63 @@ pub struct MemoizerMessage {
     p: Value,  // payload
 }
 
-pub fn respond(message: &str, stream: &mut RefMut<'_, TcpStream>) {
-    let paylaod = format!("{}\n\r", message);
-    stream.write_all(paylaod.as_bytes());
-}
-
 // Performs the corresponding action for the given
-// MemoizerMessage and responds its answer using the given stream.
-pub fn route(message: MemoizerMessage, mut stream: &mut RefMut<'_, TcpStream>) {
+// MemoizerMessage and returns the corresponding answer
+pub fn route(message: MemoizerMessage) -> String {
     match message.s.as_str() {
         "get" => {
             let key = message.p["k"].to_string();
             let value = get(key);
             match value {
-                None => respond("null", &mut stream),
-                Some(v) => respond(&v, &mut stream),
+                None => "null".to_string(),
+                Some(v) => v,
             }
         }
         "set" => {
             let key = message.p["k"].to_string();
             let value = message.p["v"].to_string();
             set(key, value);
-            respond("ok", &mut stream)
+            "ok".to_string()
         }
         "reset" => {
             reset();
-            respond("ok", &mut stream)
+            "ok".to_string()
         }
         "size" => {
             let size = size();
-            respond(&format!("{}", size), &mut stream)
+            format!("{}", size)
         }
         _ => {
             println!("Received and unsupported value");
-            respond(&format!("nok: {:?}", message), &mut stream)
+            format!("nok: {:?}", message)
         }
     }
 }
 
 // Handler that responds to the MemoizerMessage on the given line and stream.
-fn on_line_received(message: Result<MemoizerMessage>, stream: RefMut<TcpStream>) {
+fn on_line_received(message: serde_json::Result<MemoizerMessage>) -> String {
     match message {
-        Ok(m) => route(m, &mut stream),
+        Ok(m) => route(m),
         Err(err) => {
             println!("Received and unsupported value");
             let error_message = format!("{:?}", err);
-            respond(&error_message, &mut stream)
+            error_message
         }
     }
 }
 
 // Handler for a new incoming connection.
 // Will parse content as one MemoizerMessage per line.
-pub async fn on_socket_accept(stream: TcpStream) -> std::io::Result<()> {
-    let mut read = String::new();
-    let s = RefCell::new(stream);
-    let mut stream1 = s.borrow_mut();
-    let mut lines = BufReader::new(&mut *stream1).lines();
+pub async fn on_socket_accept(mut stream: TcpStream) -> Result<(), Box<dyn Error>> {
+    let (reader, mut writer) = stream.split();
+    let mut lines = BufReader::new(reader).lines();
     while let Some(line) = lines.next_line().await? {
-        read = line.clone();
+        let message: serde_json::Result<MemoizerMessage> = serde_json::from_str(&line);
+        let response = on_line_received(message);
+        let paylaod = format!("{}\n\r", response);
+        writer.write_all(paylaod.as_bytes())
+            .await
+            .expect("Failed to write to the socket");
     };
-    drop(stream1);
-    let mut stream2 = s.borrow_mut();
-    let message: Result<MemoizerMessage> = serde_json::from_str(&read);
-    // on_line_received(message, stream2);
-
-        // while let Some(line) = lines.next_line().await? {
-    //     let message: Result<MemoizerMessage> = serde_json::from_str(&line);
-    //     let mut stream2 = stream.borrow_mut();
-    //     on_line_received(message, &mut stream2);
-    // };
     Ok(())
 }
